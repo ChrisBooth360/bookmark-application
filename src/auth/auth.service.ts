5fd926948fd221c59d34f4a05a4c23766f79bfb2
chrisbooth <chris.booth.ct@gmail.com>
2023-09-24 11:17:24 +0200

    Automate postgres restart & prisma migrations

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,37 +6,54 @@ import { PrismaClientKnownRequestError } from "@prisma/client/runtime/library";
 
 @Injectable()
 export class AuthService {
-    constructor(private prisma: PrismaService){}
-    async signup(dto: AuthDto){
+    constructor(private prisma: PrismaService) { }
+    async signup(dto: AuthDto) {
         // generate the password hash
         const hash = await argon.hash(dto.password);
         // save the new user in the database
 
-        try{
+        try {
             const user = await this.prisma.user.create({
                 data: {
                     email: dto.email,
                     hash,
                 },
             });
-    
+
             delete user.hash
-    
+
             //return the saved user
             return user
         } catch (error) {
-            if (error instanceof PrismaClientKnownRequestError){
-                if(error.code === 'P2002'){
+            if (error instanceof PrismaClientKnownRequestError) {
+                if (error.code === 'P2002') {
                     throw new ForbiddenException('Credentials taken')
                 }
             }
             throw error
         }
 
-        
+
 
     }
-    signin(){
-        return {msg: "I am signed in"};
+    async signin(dto: AuthDto) {
+        // find the user by email
+        const user = await this.prisma.user.findUnique({
+            where: {
+                email: dto.email,
+            },
+        });
+
+        // if user does not exist, throw exception
+        if (!user) throw new ForbiddenException("Credentials incorrect.")
+
+        // compare password
+        const pwMatches = await argon.verify(user.hash, dto.password);
+        // if password incorrect throw exception
+        if (!pwMatches) throw new ForbiddenException("Credentials incorrect.")
+
+        // send back the user
+        delete user.hash;
+        return user
     }
 }
