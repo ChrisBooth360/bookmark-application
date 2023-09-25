7e5c6de716d074270aa2cadf464374dffeee7669
chrisbooth <chris.booth.ct@gmail.com>
2023-09-25 10:39:37 +0200

    User functionality & e2e tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -4,6 +4,7 @@ import { INestApplication, ValidationPipe } from '@nestjs/common'
 import { PrismaService } from '../src/prisma/prisma.service'
 import * as pactum from 'pactum'
 import { AuthDto } from '../src/auth/dto'
+import { EditUserDto } from 'src/user/dto'
 
 describe('App e2e', () => {
   let app: INestApplication
@@ -88,7 +89,17 @@ describe('App e2e', () => {
       })
     })
 
-    describe('Edit user', () => { })
+    describe('Edit user', () => {
+      const dto: EditUserDto = {
+        firstName: "Chris",
+        email: "chris2@gmail.com"
+      }
+      it('should edit user', () => {
+        return pactum.spec().patch('/users').withHeaders({
+          Authorization: 'Bearer $S{userAt}'
+        }).withBody(dto).expectStatus(200).expectBodyContains(dto.firstName).expectBodyContains(dto.email)
+      })
+    })
   })
 
   describe('Bookmarks', () => {
@@ -98,9 +109,9 @@ describe('App e2e', () => {
 
     describe('Get bookmark by id', () => { })
 
-    describe('Edit bookmark', () => { })
+    describe('Edit bookmark by id', () => { })
 
-    describe('Delete bookmark', () => { })
+    describe('Delete bookmark by id', () => { })
   })
 
 
